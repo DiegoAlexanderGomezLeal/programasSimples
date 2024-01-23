76866af9e358e930c8f84523cf57f1896ee22d3a
DiegoAlexanderGomezLeal <156440389+DiegoAlexanderGomezLeal@users.noreply.github.com>
2024-01-23 07:17:54 -0500

    programasSimples

--- Huevos a la copa.py
+++ Huevos a la copa.py
@@ -3,18 +3,18 @@ TH = float(input("Temperatura inicial del huevo: "))
 T = 20
 TE = 100
 M = 47
-densidad = 1.038
+D = 1.038
 C = 3.7
 K = 0.0054
 
-dividendo = (math.pow(M, (2/3)) * ( C * math.pow(densidad,(1/3))))
-divisor = (K * math.pow(math.pi, 2)) * math.pow((math.pi) / 3, (2/3))
+dividendo = (math.pow(M, (2/3)) * ( C * math.pow(D,(1/3))))
+divisor = (K * math.pow(math.pi, 2)) * math.pow((4*math.pi) / 3, (2/3))
 resultado = dividendo/divisor
-resultado2 = math.log(0.76 ((TH - TE) / (70 - TE)))
+resultado2 = math.log(0.76*((TH - TE)/(70 - TE)))
 segundos = resultado * resultado2
 minutos = round(segundos/60)
 
-print(f"El tiempo máximo para prepararlo a la copa {round(segundos)} seg")
+print(f"El tiempo máximo para prepararlo a la copa {segundos} seg")
 print(f"El tiempo máximo para prepararlo a la copa {minutos} min")
 
 
